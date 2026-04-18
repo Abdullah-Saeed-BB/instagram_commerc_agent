5c7e29ab3cc693cf2d5eb6e364bfe55aa35d64fe
Abdullah Saeed <bnbshrbconect@gmail.com>
2026-04-18 17:58:50 +0300

    Update payment_status of booking if didn't changed after paying

--- backend/routers/booking.py
+++ backend/routers/booking.py
@@ -20,6 +20,13 @@ async def get_booking(id: str, db: Session = Depends(get_db)):
 
         # Fetch payment intent from stripe to get client_secret
         intent = stripe.PaymentIntent.retrieve(booking.payment_id)
+        print(intent.status)
+        if intent.status == "succeeded" and booking.payment_status != "SUCCESSFUL":
+            booking.payment_status = "SUCCESSFUL"
+            await db.commit()
+        elif intent.status == "canceled" and (booking.payment_status != "FAILED" or booking.payment_status != "CANCELED"):
+            booking.payment_status = "FAILED"
+            await db.commit()
         
         return {
             "id": str(booking.id),
